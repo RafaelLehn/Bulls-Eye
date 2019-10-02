0e26f9feebe8c43ea9b164066c195c864c2d18f0
Rafael Lehn <rbatista@webeleven.com.br>
2019-10-02 12:06:52 -0300

    Bulls Eye

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Webeleven on 02/09/19.
+//  Copyright © 2019 Webeleven. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}
